cc391f308d932dc0e1e9a5a93c8dd724769a7741
Keshen <njkeshen98@gmail.com>
2024-09-18 04:16:15 +0800

    Checks for used email when signing up

--- includes/auth/signup.php
+++ includes/auth/signup.php
@@ -23,18 +23,39 @@
         exit;
     // Update the database with the new user and their details if all above checks have passed
     } else {
+        // Check if email has already been used before
         // SQL Command (Recipe)
-        $sql = "INSERT INTO users (`name`, `email`, `password`) VALUES (:name, :email, :password)";
+        $sql = "SELECT * FROM users WHERE email = :email";
         // Prepare SQL query (Prepare Ingredients)
         $query = $database->prepare($sql);
         // Execute SQL query (Cook)
         $query->execute([
-            'name' => $name,
-            'email' => $email,
-            'password' => password_hash($password, PASSWORD_DEFAULT)
+            'email' => $email
         ]);
-        // Redirect user back to login.php after the process
-        echo '<script>alert("Successfully signed up!");window.location.href="/login"</script>';
-        exit;
+        // Fetch (Eat)
+        $user = $query->fetch(); // Returns the first row starting from the query row
+        // Send alert message if email already exists
+        if($user){
+            $_SESSION['error'] = "Email already in use. Please use another email!";
+            header("Location: /signup");
+            exit;
+        }
+        // Process sign up if otherwise
+        else {
+            // SQL Command (Recipe)
+            $sql = "INSERT INTO users (`name`, `email`, `password`) VALUES (:name, :email, :password)";
+            // Prepare SQL query (Prepare Ingredients)
+            $query = $database->prepare($sql);
+            // Execute SQL query (Cook)
+            $query->execute([
+                'name' => $name,
+                'email' => $email,
+                'password' => password_hash($password, PASSWORD_DEFAULT)
+            ]);
+            // Redirect user back to login.php after the process
+            $_SESSION['error'] = "Successfully signed up!";
+            header("Location: /login");
+            exit;
+        }
     }
 ?>
